95ef7afb9ddd68679a864a79254c6d9ad9a61215
Ak9500 <116661624+Ak9500@users.noreply.github.com>
2022-12-04 20:19:02 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,38 @@
+// var resume = [{
+//     "name":"manikandan",
+//     "address":"none",
+//     "email":"manikandanpm880@gmail.com",
+// },
+
+// {
+//     "batch":"2019",
+//     "experience":"2 year",
+//     "skils":"full stack developer"
+// }]
+// console.log(resume)
+
+
+// var a=[2]
+// for(i=0; i<a.length; i++){
+//  console.log(a)
+// }
+
+// const name="mani"
+// const place="chennai"
+
+// const user={
+//     name,
+//     place,
+
+// }
+// console.log(user);
+
+
+function sum(a,) {
+ var result = a
+ return result    
+}
+console.log(sum(2))
+const number =[2,4,6]
+
+console.log(...number) 
